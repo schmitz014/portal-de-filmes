eecf23eac27ef2bcdc0d95bd43539f64eb775107
Pedro Schmitz <pedroschmitz0000@gmail.com>
2024-10-18 00:21:23 -0300

    feat: add endpoints basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { Outlet } from "react-router-dom";
+
 export default function App() {
   return (
-    <h1 className="text-6xl font-evers m-10 text-center">
-      Portal de Filmes
-    </h1>
+    <>
+      <h1 className="text-6xl font-evers m-10 text-center">
+        Portal de Filmes
+      </h1>
+      <Outlet />
+    </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: '*', element: <PageNotFound /> }
+    ]
+
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
