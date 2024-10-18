51b2db1fdf4eaf4af01cee5010b751e014d6a6b6
Pedro Schmitz <pedroschmitz0000@gmail.com>
2024-10-18 18:29:36 -0300

    feat: add header

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() { 
+    return (
+        <>
+            <footer className=" text-light-color text-center p-5">
+                <p>
+                </p>
+            </footer>
+        </>
+    )
+}
