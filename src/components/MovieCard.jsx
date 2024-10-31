0c67e61c0e020c28fb37b516980224c12c8724b7
Pedro Schmitz <pedroschmitz0000@gmail.com>
2024-10-31 20:53:16 -0300

    feat: new carousel add

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function MovieCard({ id, title, poster_path, backdrop_path }) {
     return (
-        <div className="relative w-72 p-2 rounded-lg overflow-hidden shadow-lg hover:scale-105 transition-all">
+        <div className="relative w-80 h-96 rounded-lg overflow-hidden shadow-lg hover:scale-105 transition-all">
             <img
                 src={`https://image.tmdb.org/t/p/w1280${backdrop_path}`}
                 alt={`${title} backdrop`}
@@ -12,13 +12,13 @@ export default function MovieCard({ id, title, poster_path, backdrop_path }) {
             <img
                 src={`https://image.tmdb.org/t/p/w154${poster_path}`}
                 alt={`${title} poster`}
-                className="relative z-10 w-56 mx-auto my-4 rounded-lg shadow-md"
+                className="relative z-10 w-36 mx-auto my-4 rounded-lg shadow-md"
             />
             
-            <div className="relative z-20 text-center text-wrap text-light-green">
-                <h1 className="font-evers text-2xl">{title}</h1>
-                <div className="mt-10">
-                    <Link to={`/movies/${id}`} className="font-nexa text-2xl"><h3 className="hover:scale-105 hover:font-bold hover: transition-all">Saber mais</h3></Link>
+            <div className="relative z-20 text-center text-light-color">
+                <h1 className="font-evers text-xl">{title}</h1>
+                <div>
+                        <Link to={`/movies/${id}`} className="font-nexa text-md"><h3 className="hover:scale-105 hover:font-bold hover:text-light-green transition-all">Saber mais</h3></Link>
                 </div>
             </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,30 @@
 import { useEffect, useState } from "react";
 import CardContainer from "../components/CardContainer";
 import MovieCard from "../components/MovieCard";
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
 
 export default function Home() {
     const [filmesPopulares, setFilmesPopulares] = useState([]);
     const [filmesTrending, setFilmesTrending] = useState([]);
     const [filmesUpcoming, setFilmesUpcoming] = useState([]);
+    const [currentSlidePopulares, setCurrentSlidePopulares] = useState(0);
+    const [currentSlideTrending, setCurrentSlideTrending] = useState(0);
+    const [currentSlideUpcoming, setCurrentSlideUpcoming] = useState(0);
+
+    const itemsPerPage = 4; // Número de filmes visíveis por slide
 
     const fetchMovies = async () => {
         try {
-            // Juntando todos os fetches
             const [respostaPopulares, respostaTrending, respostaUpcoming] = await Promise.all([
                 fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_API_KEY}&language=pt-br`),
                 fetch(`https://api.themoviedb.org/3/trending/all/week?api_key=${import.meta.env.VITE_API_KEY}&language=pt-br`),
                 fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${import.meta.env.VITE_API_KEY}&language=pt-br`)
             ]);
 
-            // Convertendo em JSON
             const popularData = await respostaPopulares.json();
             const trendingData = await respostaTrending.json();
             const upcomingData = await respostaUpcoming.json();
 
-            // Atualizar o estado apenas se os dados tiverem a estrutura correta
             setFilmesPopulares(popularData.results || []);
             setFilmesTrending(trendingData.results || []);
             setFilmesUpcoming(upcomingData.results || []);
@@ -34,31 +37,128 @@ export default function Home() {
         fetchMovies();
     }, []);
 
+    const handleNext = (setCurrentSlide, currentSlide, movies) => {
+        if (currentSlide < Math.ceil(movies.length / itemsPerPage) - 1) {
+            setCurrentSlide((prev) => prev + 1);
+        }
+    };
+
+    const handlePrev = (setCurrentSlide, currentSlide) => {
+        if (currentSlide > 0) {
+            setCurrentSlide((prev) => prev - 1);
+        }
+    };
+
     return (
         <div className="space-y-5">
+            {/* Carrossel Populares */}
             <CardContainer title="Populares">
-              {
-                filmesPopulares?.map(filme => (
-                    <MovieCard key={filme.id} {...filme}/>
-                ))
-              }
+                <div className="relative">
+                    <button 
+                        onClick={() => handlePrev(setCurrentSlidePopulares, currentSlidePopulares)} 
+                        className="text-3xl hover:-translate-x-5 bg-medium-green hover:bg-green-300 hover:scale-95 transition-all absolute left-10 top-1/2 transform -translate-y-1/2 text-white p-2 rounded-full z-10"
+                        disabled={currentSlidePopulares === 0}
+                        aria-label="Slide anterior"
+                    >
+                        <FaArrowAltCircleLeft />
+                    </button>
+                    <div className="flex justify-center w-screen">
+                        <div 
+                            className="flex transition-transform duration-300"
+                            style={{ transform: `translateX(-${currentSlidePopulares * itemsPerPage * 1}%)` }}
+                        >
+                            {filmesPopulares
+                                .slice(currentSlidePopulares * itemsPerPage, (currentSlidePopulares + 1) * itemsPerPage)
+                                .map(filme => (
+                                    <div className="mx-2 gap-10" key={filme.id}>
+                                        <MovieCard {...filme} />
+                                    </div>
+                                ))}
+                        </div>
+                    </div>
+                    <button 
+                        onClick={() => handleNext(setCurrentSlidePopulares, currentSlidePopulares, filmesPopulares)} 
+                        className="text-3xl hover:translate-x-5 bg-medium-green hover:bg-green-300 hover:scale-95 transition-all absolute right-10 top-1/2 transform -translate-y-1/2 text-white p-2 rounded-full z-10"
+                        disabled={currentSlidePopulares >= Math.ceil(filmesPopulares.length / itemsPerPage) - 1}
+                        aria-label="Próximo slide"
+                    >
+                        <FaArrowAltCircleRight />
+                    </button>
+                </div>
             </CardContainer>
 
+            {/* Carrossel Trending */}
             <CardContainer title="Trending">
-                {
-                    filmesTrending?.map(filme => (
-                        <MovieCard key={filme.id} {...filme} />
-                    ))
-                }
+                <div className="relative">
+                    <button 
+                        onClick={() => handlePrev(setCurrentSlideTrending, currentSlideTrending)} 
+                        className="text-3xl hover:-translate-x-5 bg-medium-green hover:bg-green-300 hover:scale-95 transition-all absolute left-10 top-1/2 transform -translate-y-1/2 text-white p-2 rounded-full z-10"
+                        disabled={currentSlideTrending === 0}
+                        aria-label="Slide anterior"
+                    >
+                        <FaArrowAltCircleLeft />
+                    </button>
+                    <div className="flex justify-center w-screen">
+                        <div 
+                            className="flex transition-transform duration-300"
+                            style={{ transform: `translateX(-${currentSlideTrending * itemsPerPage * 1}%)` }}
+                        >
+                            {filmesTrending
+                                .slice(currentSlideTrending * itemsPerPage, (currentSlideTrending + 1) * itemsPerPage)
+                                .map(filme => (
+                                    <div className="mx-2 gap-10" key={filme.id}>
+                                        <MovieCard {...filme} />
+                                    </div>
+                                ))}
+                        </div>
+                    </div>
+                    <button 
+                        onClick={() => handleNext(setCurrentSlideTrending, currentSlideTrending, filmesTrending)} 
+                        className="text-3xl hover:translate-x-5 bg-medium-green hover:bg-green-300 hover:scale-95 transition-all absolute right-10 top-1/2 transform -translate-y-1/2 text-white p-2 rounded-full z-10"
+                        disabled={currentSlideTrending >= Math.ceil(filmesTrending.length / itemsPerPage) - 1}
+                        aria-label="Próximo slide"
+                    >
+                        <FaArrowAltCircleRight />
+                    </button>
+                </div>
             </CardContainer>
 
+            {/* Carrossel Próximos Lançamentos */}
             <CardContainer title="Próximos Lançamentos">
-                {
-                    filmesUpcoming?.map(filme => (
-                        <MovieCard key={filme.id} {...filme} />
-                    ))
-                }
+                <div className="relative">
+                    <button 
+                        onClick={() => handlePrev(setCurrentSlideUpcoming, currentSlideUpcoming)} 
+                        className="text-3xl hover:-translate-x-5 bg-medium-green hover:bg-green-300 hover:scale-95 transition-all absolute left-10 top-1/2 transform -translate-y-1/2 text-white p-2 rounded-full z-10"
+                        disabled={currentSlideUpcoming === 0}
+                        aria-label="Slide anterior"
+                    >
+                        <FaArrowAltCircleLeft />
+                    </button>
+                    <div className="flex justify-center w-screen">
+                        <div 
+                            className="flex transition-transform duration-300"
+                            style={{ transform: `translateX(-${currentSlideUpcoming * itemsPerPage * 1}%)` }}
+                        >
+                            {filmesUpcoming
+                                .slice(currentSlideUpcoming * itemsPerPage, (currentSlideUpcoming + 1) * itemsPerPage)
+                                .map(filme => (
+                                    <div className="mx-2 gap-10" key={filme.id}>
+                                        <MovieCard {...filme} />
+                                    </div>
+                                ))}
+                        </div>
+                    </div>
+                    <button 
+                        onClick={() => handleNext(setCurrentSlideUpcoming, currentSlideUpcoming, filmesUpcoming)} 
+                        className="text-3xl hover:translate-x-5 bg-medium-green hover:bg-green-300 hover:scale-95 transition-all absolute right-10 top-1/2 transform -translate-y-1/2 text-white p-2 rounded-full z-10"
+                        disabled={currentSlideUpcoming >= Math.ceil(filmesUpcoming.length / itemsPerPage) - 1}
+                        aria-label="Próximo slide"
+                    >
+                        <FaArrowAltCircleRight />
+                    </button>
+                </div>
             </CardContainer>
+
         </div>
     );
 }
