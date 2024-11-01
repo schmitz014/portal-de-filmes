ed3ca1fda3a233f4dea746749c62f10ee188c52c
Pedro Schmitz <pedroschmitz0000@gmail.com>
2024-10-31 21:26:57 -0300

    fix: fix page 404

--- src/pages/PageNotFOund.jsx
+++ src/pages/PageNotFOund.jsx
@@ -3,7 +3,7 @@ import { TbError404Off } from "react-icons/tb"
 export default function PageNotFound() {
     return (
         <>
-        <div className="flex flex-col justify-center items-center ml-48 gap-10 text-4xl font-evers">
+        <div className="flex flex-col h-screen justify-center items-center gap-10 text-4xl font-evers">
             <h1 className="text-9xl">
                 <TbError404Off />
             </h1>
