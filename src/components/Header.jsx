1124c18cc2569b059bc35b33b924f4ba4bef7c4f
Pedro Schmitz <pedroschmitz0000@gmail.com>
2024-11-02 08:12:14 -0300

    fix: fix header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,13 +15,13 @@ export default function Header() {
             <header className="flex justify-around bg-gradient-custom2 items-center">
                 <div className="flex items-center pl-8 gap-10 hover:scale-110 transition-all">
                     <img className="w-16" src="/movie.svg" alt="icon movie" />
-                    <NavLink to="/"><h1 className="font-evers text-5xl text-gray-custom3">Portal Filmes</h1></NavLink>
+                    <NavLink to="/"><h1 className="font-evers text-4xl text-gray-custom3">Portal Filmes</h1></NavLink>
                 </div>
                 <nav>
                     <ul className="font-nexa text-4xl flex">
-                        <li className="hover:scale-110 hover:font-bold text-gray-custom3 transition-all m-10"><NavLink to="/">Home</NavLink></li>
-                        <li className="hover:scale-110 hover:font-bold text-gray-custom3 transition-all m-10"><NavLink to="/movies">Filmes</NavLink></li>
-                        <li className="hover:scale-110 hover:font-bold text-gray-custom3 transition-all m-10"><NavLink to="/genres">Gênero</NavLink></li>
+                        <li className="hover:font-bold text-gray-custom3 transition-all m-10"><NavLink to="/">Home</NavLink></li>
+                        <li className="hover:font-bold text-gray-custom3 transition-all m-10"><NavLink to="/movies">Filmes</NavLink></li>
+                        <li className="hover:font-bold text-gray-custom3 transition-all m-10"><NavLink to="/genres">Gênero</NavLink></li>
                     </ul>
                 </nav>
                 <Login isLogged={isLogged} handleLogin={handleLogin} />
